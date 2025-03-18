818f0919f96d7fe85857daa42c4b9e192ebbcc4e
JorgePastorCastilla <jordi131313@gmail.com>
2025-03-18 13:18:55 +0100

    added photon and networkmanager on mainmenu

---
+++ Assets/Scripts/NetworkingManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Photon.Realtime;
+public class NetworkingManager : MonoBehaviourPunCallbacks
+{
+
+    public Button multiplayerButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Unir-mos a un Lobby");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Estam apunt per jugar!");
+        multiplayerButton.interactable = true;
+    }
+
+    public void FindMatch()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        MakeRoom();
+    }
+
+    public void MakeRoom()
+    {
+     RoomOptions roomOptions = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 6, PublishUserId = true};
+     var randomRoomName = 1;
+     PhotonNetwork.CreateRoom($"RoomName_{randomRoomName}", roomOptions);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(2);
+        
+        // base.OnJoinedRoom();
+    }
+}
